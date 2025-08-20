c68477a5e87382b540f9aef07e3f7a10682527d9
André <114795980+Techh-Priest@users.noreply.github.com>
2025-08-21 01:19:41 +0300

    Update day_20.py

--- day_20.py
+++ day_20.py
@@ -1,8 +1,11 @@
-        #### Snake Game from Nokia 3310!!!
+#### Snake Game from Nokia 3310!!!
 
-from turtle import *
+from turtle import Screen
 import time
 from snake import *
+from food import *
+from scoreboard import *
+
 
 screen = Screen()
 screen.setup(width=600, height=600)
@@ -11,12 +14,15 @@ screen.title("Andre's Snake Game")
 screen.tracer(0)
 
 anaconda = Snake()
+food = Food()
+scoreboard = ScoreBoard()
+
+
 screen.listen()
 screen.onkey(anaconda.up, "Up")
 screen.onkey(anaconda.down, "Down")
 screen.onkey(anaconda.left, "Left")
 screen.onkey(anaconda.right, "Right")
-
 
 
 game_on = True
@@ -25,8 +31,25 @@ while game_on == True:
     time.sleep(0.1)
 
     anaconda.move()
+    # Detect Collision with food.
+    if anaconda.head.distance(food) < 15:
+        food.refresh()
+        anaconda.extend()
+        scoreboard.count_score()
+
+    # Detect Collision with wall
+    if anaconda.head.xcor() > 280 or anaconda.head.xcor() < -280 or anaconda.head.ycor() > 280 or anaconda.head.ycor() < -280:
+        game_on = False
+        scoreboard.game_over()
+
+    # Detect Collision with own Tail
+    for snake in anaconda.snake[1:]:
+        if anaconda.head.distance(snake) < 10:
+            game_on = False
+            scoreboard.game_over()
 
 
+print(anaconda.extend())
 
 
 
