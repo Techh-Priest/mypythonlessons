d9be93efe5466de6a509081b222c11ee579dc730
André <114795980+Techh-Priest@users.noreply.github.com>
2025-09-01 23:09:04 +0300

    Add files via upload

---
+++ main.py
@@ -0,0 +1,48 @@
+import csv
+import turtle
+from turtle import Turtle
+import pandas
+
+screen = turtle.Screen()
+screen.title("U.S. States Game")
+image = "blank_states_img.gif"
+screen.addshape(image)
+turtle.shape(image)
+
+df = pandas.read_csv("50_states.csv")
+
+all_states = df.state.to_list()
+guessed_states = []
+secret_word = "Exit"
+
+while len(guessed_states) < 50:
+    answer_state = screen.textinput(title=f"{len(guessed_states)}/50 states correct", prompt="What's another state name?:").title()
+    if answer_state == secret_word:
+        missing_states = []
+        for state in all_states:
+            if state not in guessed_states:
+                missing_states.append(state)
+        new_data = pandas.DataFrame(missing_states)
+        new_data.to_csv("states_to_learn.csv")
+        break
+    if answer_state in all_states:
+        guessed_states.append(answer_state)
+        t = turtle.Turtle()
+        t.hideturtle()
+        t.penup()
+        state_data = df[df.state == answer_state]
+        t.goto(int(state_data.x), int(state_data.y))
+        t.write(answer_state)
+
+
+
+
+
+
+
+
+# def get_mouse_click_coor(x, y):
+#     print(x, y)
+#
+# turtle.onscreenclick(get_mouse_click_coor)
+#turtle.mainloop()
