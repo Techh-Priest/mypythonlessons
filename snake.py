07cfc156c82673457eb819792f37f0aeedc43a7d
André <114795980+Techh-Priest@users.noreply.github.com>
2025-09-22 20:22:10 +0300

    Update snake.py

--- snake.py
+++ snake.py
@@ -1,7 +1,5 @@
-from turtle import *
-import time
-
-STARTING_POS = [0, -20, -40]
+from turtle import Turtle
+STARTING_POSITIONS = [(0, 0), (-20, 0), (-40, 0)]
 MOVE_DISTANCE = 20
 UP = 90
 DOWN = 270
@@ -9,59 +7,52 @@ LEFT = 180
 RIGHT = 0
 
 class Snake:
-
     def __init__(self):
-        self.snake = []
+        self.segments = []
         self.create_snake()
-        self.head = self.snake[0]
+        self.head = self.segments[0]
 
     def create_snake(self):
-        for square in range(0, 3):
-            body = Turtle()
-            body.penup()
-            body.shape("square")
-            body.goto(x=STARTING_POS[square], y=0) # bug is here
-            body.color("white")
-            self.snake.append(body)
-            #self.add_segment(square)
+        for position in STARTING_POSITIONS:
+            self.add_segment(position)
 
     def move(self):
-        for seg_num in range(len(self.snake) - 1, 0, -1):
-            new_x = self.snake[seg_num - 1].xcor()
-            new_y = self.snake[seg_num - 1].ycor()
-            self.snake[seg_num].goto(new_x, new_y)
+        for seg_num in range(len(self.segments) - 1, 0, -1):
+            new_x = self.segments[seg_num - 1].xcor()
+            new_y = self.segments[seg_num - 1].ycor()
+            self.segments[seg_num].goto(new_x, new_y)
         self.head.forward(MOVE_DISTANCE)
-
 
     def up(self):
         if self.head.heading() != DOWN:
             self.head.setheading(UP)
 
-
     def down(self):
         if self.head.heading() != UP:
             self.head.setheading(DOWN)
-
-
-    def right(self):
-        if self.head.heading() != LEFT:
-            self.head.setheading(RIGHT)
-
 
     def left(self):
         if self.head.heading() != RIGHT:
             self.head.setheading(LEFT)
 
+    def right(self):
+        if self.head.heading() != LEFT:
+            self.head.setheading(RIGHT)
 
-    def add_segment(self, square):
-        pass
-        # body = Turtle()
-        # body.penup()
-        # body.shape("square")
-        # body.goto(x=STARTING_POS[square], y=0) # bug is here
-        # body.color("white")
-        # self.snake.append(body)
+    def add_segment(self, position):
+        new_segment = Turtle(shape="square")
+        new_segment.color("white")
+        new_segment.penup()
+        new_segment.goto(position)
+        self.segments.append(new_segment)
 
     def extend(self):
-        pass
-#        self.add_segment(self.snake[-1].position()) # bug is here
+        #add new segement to feeding snake
+        self.add_segment(self.segments[-1].position())
+
+    def reset(self):
+        for seg in self.segments:
+            seg.goto(2000, 2000)
+        self.segments.clear()
+        self.create_snake()
+        self.head = self.segments[0]
